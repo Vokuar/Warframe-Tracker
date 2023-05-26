d1318f959d130f016979ec4623603fee9cfea6d4
Vokuar <mindblast213@gmail.com>
2023-05-26 10:26:34 +1000

    Update warframe_tracker.js

--- js/warframe_tracker.js
+++ js/warframe_tracker.js
@@ -15,12 +15,30 @@ function addWeapon() {
   displayWeapons();
 }
 
+function removeWarframe(index) {
+  warframes.splice(index, 1);
+  displayWarframes();
+}
+
+function removeWeapon(index) {
+  weapons.splice(index, 1);
+  displayWeapons();
+}
+
 function displayWarframes() {
   var warframesList = document.getElementById("warframes-list");
   warframesList.innerHTML = "";
   for (var i = 0; i < warframes.length; i++) {
     var warframeItem = document.createElement("li");
+    var removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.onclick = (function(index) {
+      return function() {
+        removeWarframe(index);
+      };
+    })(i);
     warframeItem.textContent = warframes[i];
+    warframeItem.appendChild(removeButton);
     warframesList.appendChild(warframeItem);
   }
 }
@@ -30,7 +48,31 @@ function displayWeapons() {
   weaponsList.innerHTML = "";
   for (var i = 0; i < weapons.length; i++) {
     var weaponItem = document.createElement("li");
+    var removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.onclick = (function(index) {
+      return function() {
+        removeWeapon(index);
+      };
+    })(i);
     weaponItem.textContent = weapons[i];
+    weaponItem.appendChild(removeButton);
     weaponsList.appendChild(weaponItem);
   }
+}
+
+function getDataFromWiki(wikiUrl) {
+  axios.get(wikiUrl)
+    .then(function(response) {
+      // Parse the response data and extract the required information
+      // You'll need to inspect the HTML structure of the Warframe Wiki to determine the data extraction process
+      // Update the code here to extract the desired information from the response data
+      var data = response.data;
+      
+      // Process the data as needed
+      // ...
+    })
+    .catch(function(error) {
+      console.log("An error occurred while retrieving data from the Warframe Wiki:", error);
+    });
 }
