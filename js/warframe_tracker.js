2abefd54b7b55b2c181c06e62560346783d22f5d
Vokuar <mindblast213@gmail.com>
2023-05-26 10:32:28 +1000

    Update warframe_tracker.js

--- js/warframe_tracker.js
+++ js/warframe_tracker.js
@@ -7,22 +7,30 @@ function addWarframe() {
   var warframeName = prompt("Enter the Warframe name:");
   warframes.push(warframeName);
   displayWarframes();
+  saveDataToCookies();
+  saveDataToLocalStorage();
 }
 
 function addWeapon() {
   var weaponName = prompt("Enter the Weapon name:");
   weapons.push(weaponName);
   displayWeapons();
+  saveDataToCookies();
+  saveDataToLocalStorage();
 }
 
 function removeWarframe(index) {
   warframes.splice(index, 1);
   displayWarframes();
+  saveDataToCookies();
+  saveDataToLocalStorage();
 }
 
 function removeWeapon(index) {
   weapons.splice(index, 1);
   displayWeapons();
+  saveDataToCookies();
+  saveDataToLocalStorage();
 }
 
 function displayWarframes() {
@@ -61,18 +69,62 @@ function displayWeapons() {
   }
 }
 
-function getDataFromWiki(wikiUrl) {
-  axios.get(wikiUrl)
-    .then(function(response) {
-      // Parse the response data and extract the required information
-      // You'll need to inspect the HTML structure of the Warframe Wiki to determine the data extraction process
-      // Update the code here to extract the desired information from the response data
-      var data = response.data;
-      
-      // Process the data as needed
-      // ...
-    })
-    .catch(function(error) {
-      console.log("An error occurred while retrieving data from the Warframe Wiki:", error);
-    });
+function saveDataToCookies() {
+  document.cookie = "warframes=" + JSON.stringify(warframes);
+  document.cookie = "weapons=" + JSON.stringify(weapons);
 }
+
+function saveDataToLocalStorage() {
+  localStorage.setItem("warframes", JSON.stringify(warframes));
+  localStorage.setItem("weapons", JSON.stringify(weapons));
+}
+
+function loadDataFromCookies() {
+  var warframesCookie = getCookie("warframes");
+  if (warframesCookie) {
+    warframes = JSON.parse(warframesCookie);
+    displayWarframes();
+  }
+
+  var weaponsCookie = getCookie("weapons");
+  if (weaponsCookie) {
+    weapons = JSON.parse(weaponsCookie);
+    displayWeapons();
+  }
+}
+
+function loadDataFromLocalStorage() {
+  var warframesData = localStorage.getItem("warframes");
+  if (warframesData) {
+    warframes = JSON.parse(warframesData);
+    displayWarframes();
+  }
+
+  var weaponsData = localStorage.getItem("weapons");
+  if (weaponsData) {
+    weapons = JSON.parse(weaponsData);
+    displayWeapons();
+  }
+}
+
+function getCookie(name) {
+  var cookieName = name + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var cookieArray = decodedCookie.split(";");
+  for (var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while (cookie.charAt(0) === " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return "";
+}
+
+// Load data from cookies and local storage on page load
+window.onload = function() {
+  loadDataFromCookies();
+  loadDataFromLocalStorage();
+};
